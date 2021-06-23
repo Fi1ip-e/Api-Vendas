8c3626fb70bc31ef6b6d8dfd923d6c68b8786952
Filipe <filipe-carvalho@outlook.com>
2021-06-23 09:45:44 -0300

    PrimeiroCommit

---
+++ src/readme.md
@@ -0,0 +1,11 @@
+Instalação das bibliotecas:
+
+# yarn add express cors express-async-errors
+
+tipagem
+
+# yarn add -D @types/express @types/cors
+
+instalação do typeORM e postgres
+
+# yarn add typeorm reflect-metadata pg
---
+++ src/shared/http/server.ts
@@ -0,0 +1,35 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes/index';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(routes);
+
+//Pegar os erros. Evitar criação de try/cach
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+
+    //se a instancia do erro for da classe
+
+    if (error instanceof AppError) {
+        return res.status(error.statusCode).json({
+            status: 'error',
+            message: error.message,
+        })
+    };
+
+    // Error fora da classe
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Erro interno',
+    });
+});
+
+app.listen(3333, () => { console.log("servidor rodando na porta 3333!!") });
